14a215e93cdbcaf0676e07a7f2264171e8cc352b
aganeshkumar1222 <121375373+aganeshkumar1222@users.noreply.github.com>
2022-12-28 21:21:20 +0530

    Create variables.tf

---
+++ terraform/mongodb/variables.tf
@@ -0,0 +1,9 @@
+variable "AWS_REGION" {
+  description = "Deployment region"
+  default = "us-east-1"
+}
+
+variable "ENVIRONMENT" {
+  description = "The Deployment environment"
+  default = "dev"
+}
